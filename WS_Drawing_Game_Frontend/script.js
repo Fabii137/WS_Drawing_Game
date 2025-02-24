5fbfc9efbc72cad52ac333289aff24274fa6e072
Fabian KRSTIC <Fabian.KRSTIC@bachmann.info>
2025-02-24 13:15:43 +0100

    add colors + clear options

--- WS_Drawing_Game_Frontend/script.js
+++ WS_Drawing_Game_Frontend/script.js
@@ -1,9 +1,11 @@
-window.addEventListener('load', ()=>{ 
+window.addEventListener('load', () => { 
 	resize();
 	document.addEventListener('mousedown', startPainting); 
 	document.addEventListener('mouseup', stopPainting); 
 	document.addEventListener('mousemove', sketch); 
 	window.addEventListener('resize', resize); 
+
+
 }); 
 	
 const canvas = document.querySelector('#canvas'); 
@@ -12,11 +14,12 @@ const ctx = canvas.getContext('2d');
 	
 function resize(){ 
     ctx.canvas.width = window.innerWidth / 2; 
-    ctx.canvas.height = window.innerHeight / 1.5; 
+    ctx.canvas.height = window.innerHeight / 1.7; 
 } 
 	
 let coord = {x:0 , y:0}; 
-let paint = false; 
+let paint = false;
+let color = 'black'; 
 
 function getPosition(event){ 
     coord.x = event.clientX - canvas.offsetLeft; 
@@ -29,6 +32,14 @@ function startPainting(event){
 function stopPainting(){ 
     paint = false; 
 } 
+
+function setColor(setColor) {
+    color = setColor;
+}
+
+function clearCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
 	
 function sketch(event){ 
     if (!paint) 
@@ -37,9 +48,11 @@ function sketch(event){
     ctx.beginPath(); 
     ctx.lineWidth = 5; 
     ctx.lineCap = 'round';     
-    ctx.strokeStyle = 'red'; 
+    ctx.strokeStyle = color; 
     ctx.moveTo(coord.x, coord.y); 
     getPosition(event); 
     ctx.lineTo(coord.x , coord.y); 
     ctx.stroke(); 
 }
+
+

dd32a7f6bcd71e7e2b82a50adcce7816713ba03e
Fabian KRSTIC <Fabian.KRSTIC@bachmann.info>
2025-05-19 14:51:55 +0200

    refactored code and added documentation

--- WS_Drawing_Game_Backend/src/main/java/org/DrawingGame/GameSession.java
+++ WS_Drawing_Game_Backend/src/main/java/org/DrawingGame/GameSession.java
@@ -48,6 +48,25 @@ public class GameSession {
         }
     }
 
+    /**
+     * reads file and adds the words to an array
+     * @throws FileNotFoundException if file is not found
+     */
+    private void readWordsFile() throws FileNotFoundException {
+//        File file = new File("/home/words.txt");
+        File file = new File("words.txt");
+        Scanner scanner = new Scanner(file);
+        while(scanner.hasNext()) {
+            words.add(scanner.nextLine());
+        }
+    }
+
+
+    /**
+     * adds player to the game<br>
+     * starts the game if player count is 2
+     * @param player player to add
+     */
     public void addPlayer(Player player) {
         WebSocket ws = player.getWebSocket();
         player.setId(maxPlayerID++);
@@ -65,6 +84,11 @@ public class GameSession {
         }
     }
 
+    /**
+     * removes player from game<br>
+     * stops game if player count is below 2
+     * @param player player to remove
+     */
     public void deletePlayer(WebSocket player) {
         Player playerToRemove = getPlayerFromWebSocket(player);
         if (playerToRemove == null)
@@ -86,147 +110,12 @@ public class GameSession {
                 sendScoreboard(p);
             }
         }
-
-
     }
 
-    private void stopGame() {
-        timeService.shutdown();
-        isRunning = false;
-        strokes.clear();
-        hintPositions.clear();
-        guessedPlayers.clear();
-        broadcast("clear");
-    }
-
-    public int getGameSize() {
-        return players.size();
-    }
-
-    public void handleMessage(WebSocket ws, String message) {
-        JsonObject jsonMessage = JsonParser.parseString(message).getAsJsonObject();
-        if (!jsonMessage.has("type")) return;
-
-        String type = jsonMessage.get("type").getAsString();
-        Player author = getPlayerFromWebSocket(ws);
-
-        switch (type) {
-            case "stroke" -> handleStroke(ws, jsonMessage);
-            case "clear" -> sendClear(ws);
-            case "get_strokes" -> sendStrokes(ws);
-            case "message" -> handleChatMessage(author, jsonMessage);
-        }
-    }
-
-    private void handleStroke(WebSocket ws, JsonObject jsonMessage) {
-        if(getPlayerFromWebSocket(ws) != currentTurn)
-            return;
-
-        JsonObject data = jsonMessage.getAsJsonObject("data");
-
-        Map<String, Object> stroke = new HashMap<>();
-        stroke.put("x1", data.get("x1").getAsDouble());
-        stroke.put("y1", data.get("y1").getAsDouble());
-        stroke.put("x2", data.get("x2").getAsDouble());
-        stroke.put("y2", data.get("y2").getAsDouble());
-        stroke.put("color", data.get("color").getAsString());
-        stroke.put("width", data.get("width").getAsInt());
-        strokes.add(stroke);
-
-        broadcastBut(currentTurn, Map.of("type", "stroke", "data", stroke));
-    }
-
-    private void sendClear(WebSocket ws) {
-        if (getPlayerFromWebSocket(ws) == currentTurn) {
-            strokes.clear();
-            broadcastBut(currentTurn, Map.of("type", "clear"));
-        }
-    }
-
-    private void sendStrokes(WebSocket ws) {
-        send(ws, Map.of("type", "clear"));
-        for (Map<String, Object> stroke : strokes) {
-            send(ws, Map.of("type", "stroke", "data", stroke));
-        }
-    }
-
-    private void handleChatMessage(Player author, JsonObject jsonMessage) {
-        if (jsonMessage.has("data")) {
-            String msg = jsonMessage.get("data").getAsString();
-
-            if (msg.equalsIgnoreCase(word) && author != currentTurn && !guessedPlayers.contains(author)) {
-                processCorrectGuess(author);
-            } else {
-                broadcastBut(author, Map.of("type", "message", "data", msg, "username", author.getUsername()));
-            }
-        }
-    }
-
-    private void processCorrectGuess(Player author) {
-        broadcast(Map.of("type", "correct", "id", Integer.toString(author.getId()), "username", author.getUsername()));
-        guessedPlayers.add(author);
-
-        int points = players.size() > 2 ? (int) (BASE_POINTS * Math.pow(DECAY_FACTOR, guessedPlayers.size() - 1)) : BASE_POINTS;
-        playerToPreparedPoints.put(author, points);
-
-        if (checkDone()) {
-            endTurn();
-        }
-    }
-
-
-    private void resetGame() {
-        stopGame();
-
-        List<Player> sortedPlayers = new ArrayList<>(players);
-        sortedPlayers.sort(Comparator.comparing(Player::getPoints));
-
-        for(int i = 0; i < sortedPlayers.size(); i++) {
-            Player player = sortedPlayers.get(i);
-            String display = String.format("%d → %s: %d", sortedPlayers.size() - i, player.getUsername(), player.getPoints());
-            broadcast(Map.of("type", "message", "data", display));
-            player.resetPoints();
-        }
-        startGame();
-    }
-
-    private void sendFullGameData(Player player) {
-        WebSocket ws = player.getWebSocket();
-        send(ws, Map.of("type", "start", "name", currentTurn.getUsername(), "id", Integer.toString(currentTurn.getId()), "length", Integer.toString(word.length()), "round", currentRound, "maxRound", ROUND_COUNT));
-
-        for(Player p : players) {
-            sendScoreboard(p);
-        }
-
-        List<Map<String, String>> guessedPlayersData = new ArrayList<>();
-        for (Player p : guessedPlayers) {
-            guessedPlayersData.add(Map.of("id", Integer.toString(p.getId()), "username", p.getUsername()));
-        }
-        send(ws, Map.of("type", "guessed_players", "data", guessedPlayersData));
-
-        for (Map<String, Object> stroke : strokes) {
-            send(ws, Map.of("type", "stroke", "data", stroke));
-        }
-
-    }
-
-    private void sendScoreboard(Player player) {
-        List<Map<String, String>> scoreboardData = new ArrayList<>();
-        for(Player p : players) {
-            scoreboardData.add(Map.of("id", Integer.toString(p.getId()), "username", p.getUsername(), "points", Integer.toString(p.getPoints())));
-        }
-        send(player.getWebSocket(), Map.of("type", "scoreboard", "data", scoreboardData));
-    }
-
-    private void readWordsFile() throws FileNotFoundException {
-//        File file = new File("/home/words.txt");
-        File file = new File("words.txt");
-        Scanner scanner = new Scanner(file);
-        while(scanner.hasNext()) {
-            words.add(scanner.nextLine());
-        }
-    }
-
+    /**
+     * @param ws Websocket
+     * @return Player with the given Websocket
+     */
     private Player getPlayerFromWebSocket(WebSocket ws) {
         Player player = null;
         for(Player p : players)  {
@@ -238,10 +127,17 @@ public class GameSession {
         return player;
     }
 
-    private boolean checkDone() {
-        return guessedPlayers.size() == (players.size() - 1);
+    /**
+     * gets player count
+     * @return player count
+     */
+    public int getGameSize() {
+        return players.size();
     }
 
+    /**
+     * starts the game and broadcasts the data
+     */
     private void startGame() {
         currentTurn = players.getFirst();
         playerIdx = 0;
@@ -259,6 +155,40 @@ public class GameSession {
         send(currentTurn.getWebSocket(), Map.of("type", "word", "data", word));
     }
 
+    /**
+     * stops the game, broadcasts leaderboard and starts the next game
+     */
+    private void resetGame() {
+        stopGame();
+
+        List<Player> sortedPlayers = new ArrayList<>(players);
+        sortedPlayers.sort(Comparator.comparing(Player::getPoints));
+
+        for(int i = 0; i < sortedPlayers.size(); i++) {
+            Player player = sortedPlayers.get(i);
+            String display = String.format("%d → %s: %d", sortedPlayers.size() - i, player.getUsername(), player.getPoints());
+            broadcast(Map.of("type", "message", "data", display));
+            player.resetPoints();
+        }
+        startGame();
+    }
+
+    /**
+     * stops the game<br>
+     * (clears game state variables)
+     */
+    private void stopGame() {
+        timeService.shutdown();
+        isRunning = false;
+        strokes.clear();
+        hintPositions.clear();
+        guessedPlayers.clear();
+        broadcast("clear");
+    }
+
+    /**
+     * adds the points from the current turn to the players and starts next turn
+     */
     private void endTurn() {
         int totalPoints = 0;
         for(Player p : playerToPreparedPoints.keySet()) {
@@ -274,6 +204,9 @@ public class GameSession {
             nextTurn();
     }
 
+    /**
+     * starts next turn, resets variables and broadcasts scoreboard
+     */
     private void nextTurn() {
         timeService.shutdown();
 
@@ -305,6 +238,11 @@ public class GameSession {
         activateTimeService();
     }
 
+    /**
+     * activates timer which broadcasts the time left in the current turn <br>
+     * will send hints on 75%, 50% and 25%<br>
+     * if time left is 0, it will end the turn
+     */
     private void activateTimeService() {
         timeLeft = ROUND_DURATION;
         timeService = Executors.newScheduledThreadPool(1);
@@ -331,6 +269,156 @@ public class GameSession {
         timeService.scheduleAtFixedRate(timeRunnable, 0, 1, TimeUnit.SECONDS);
     }
 
+    /**
+     * gets called if websocket server receives a message<br>
+     * parses json and will call the function in the message if given
+     * @param ws Player which sent the message
+     * @param message Message
+     */
+    public void handleMessage(WebSocket ws, String message) {
+        JsonObject jsonMessage = JsonParser.parseString(message).getAsJsonObject();
+        if (!jsonMessage.has("type"))
+            return;
+
+        String type = jsonMessage.get("type").getAsString();
+        Player author = getPlayerFromWebSocket(ws);
+
+        switch (type) {
+            case "stroke" -> handleStroke(ws, jsonMessage);
+            case "clear" -> sendClear(ws);
+            case "get_strokes" -> sendStrokes(ws);
+            case "message" -> handleChatMessage(author, jsonMessage);
+        }
+    }
+
+    /**
+     * gets stroke and broadcasts it to other players
+     * @param ws Player which sent the message
+     * @param jsonMessage Json Message containing the stroke
+     */
+    private void handleStroke(WebSocket ws, JsonObject jsonMessage) {
+        if(getPlayerFromWebSocket(ws) != currentTurn)
+            return;
+
+        JsonObject data = jsonMessage.getAsJsonObject("data");
+
+        Map<String, Object> stroke = new HashMap<>();
+        stroke.put("x1", data.get("x1").getAsDouble());
+        stroke.put("y1", data.get("y1").getAsDouble());
+        stroke.put("x2", data.get("x2").getAsDouble());
+        stroke.put("y2", data.get("y2").getAsDouble());
+        stroke.put("color", data.get("color").getAsString());
+        stroke.put("width", data.get("width").getAsInt());
+        strokes.add(stroke);
+
+        broadcastBut(currentTurn, Map.of("type", "stroke", "data", stroke));
+    }
+
+    /**
+     * clears strokes and sends clear to other players
+     * @param ws Player which sent the message
+     */
+    private void sendClear(WebSocket ws) {
+        if (getPlayerFromWebSocket(ws) == currentTurn) {
+            strokes.clear();
+            broadcastBut(currentTurn, Map.of("type", "clear"));
+        }
+    }
+
+    /**
+     * sends all strokes that got drawn since last clear or turn change
+     * @param ws Player which sent the message
+     */
+    private void sendStrokes(WebSocket ws) {
+        send(ws, Map.of("type", "clear"));
+        for (Map<String, Object> stroke : strokes) {
+            send(ws, Map.of("type", "stroke", "data", stroke));
+        }
+    }
+
+    /**
+     * broadcasts chat message<br>
+     * also if it is the current word, it will mark them as guessed
+     * @param author Player which sent the message
+     * @param jsonMessage Message
+     */
+    private void handleChatMessage(Player author, JsonObject jsonMessage) {
+        if (jsonMessage.has("data")) {
+            String msg = jsonMessage.get("data").getAsString();
+
+            if (msg.equalsIgnoreCase(word) && author != currentTurn && !guessedPlayers.contains(author)) {
+                processCorrectGuess(author);
+            } else {
+                broadcastBut(author, Map.of("type", "message", "data", msg, "username", author.getUsername()));
+            }
+        }
+    }
+
+    /**
+     * will add the player to guessed players and calculate the points they will get <br>
+     * broadcasts message containing the guessed player
+     * @param author
+     */
+    private void processCorrectGuess(Player author) {
+        broadcast(Map.of("type", "correct", "id", Integer.toString(author.getId()), "username", author.getUsername()));
+        guessedPlayers.add(author);
+
+        int points = players.size() > 2 ? (int) (BASE_POINTS * Math.pow(DECAY_FACTOR, guessedPlayers.size() - 1)) : BASE_POINTS;
+        playerToPreparedPoints.put(author, points);
+
+        if (checkDone()) {
+            endTurn();
+        }
+    }
+
+    /**
+     * checks if all players have guessed
+     * @return result
+     */
+    private boolean checkDone() {
+        return guessedPlayers.size() == (players.size() - 1);
+    }
+
+    /**
+     * sends full game state to the player
+     * @param player Player to receive game state
+     */
+    private void sendFullGameData(Player player) {
+        WebSocket ws = player.getWebSocket();
+        send(ws, Map.of("type", "start", "name", currentTurn.getUsername(), "id", Integer.toString(currentTurn.getId()), "length", Integer.toString(word.length()), "round", currentRound, "maxRound", ROUND_COUNT));
+
+        for(Player p : players) {
+            sendScoreboard(p);
+        }
+
+        List<Map<String, String>> guessedPlayersData = new ArrayList<>();
+        for (Player p : guessedPlayers) {
+            guessedPlayersData.add(Map.of("id", Integer.toString(p.getId()), "username", p.getUsername()));
+        }
+        send(ws, Map.of("type", "guessed_players", "data", guessedPlayersData));
+
+        for (Map<String, Object> stroke : strokes) {
+            send(ws, Map.of("type", "stroke", "data", stroke));
+        }
+
+    }
+
+    /**
+     * sends scoreboard(id, name, points) to the player
+     * @param player Player to receive leaderboard
+     */
+    private void sendScoreboard(Player player) {
+        List<Map<String, String>> scoreboardData = new ArrayList<>();
+        for(Player p : players) {
+            scoreboardData.add(Map.of("id", Integer.toString(p.getId()), "username", p.getUsername(), "points", Integer.toString(p.getPoints())));
+        }
+        send(player.getWebSocket(), Map.of("type", "scoreboard", "data", scoreboardData));
+    }
+
+    /**
+     * broadcasts only the type
+     * @param type type of the message
+     */
     private void broadcast(String type) {
         String message = gson.toJson(Map.of("type", type));
         for(Player p : players) {
@@ -338,6 +426,10 @@ public class GameSession {
         }
     }
 
+    /**
+     * broadcasts full map
+     * @param objMap object map of type and values
+     */
     private void broadcast(Map<String, Object> objMap) {
         String message = gson.toJson(objMap);
         for(Player p : players) {
@@ -345,6 +437,11 @@ public class GameSession {
         }
     }
 
+    /**
+     * broadcasts full map to all players but the given player
+     * @param player player to not send to
+     * @param objMap object map of type and values
+     */
     private void broadcastBut(Player player, Map<String, Object> objMap) {
         String message = gson.toJson(objMap);
         for(Player p : players) {
@@ -353,6 +450,11 @@ public class GameSession {
         }
     }
 
+    /**
+     * send to player
+     * @param ws websocket of player to send to
+     * @param objMap object map of type and values
+     */
     private void send(WebSocket ws, Map<String, Object> objMap) {
         String message = gson.toJson(objMap);
         ws.send(message);
